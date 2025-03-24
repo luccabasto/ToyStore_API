1f51387c729984a2b0f1012d83fbf95842277550
Lucas Basto <136653403+luccabasto@users.noreply.github.com>
2025-03-24 09:55:12 -0300

    feat: pre build init

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToyStore_API.Models;
+
+namespace ToyStore_API.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Toys> Toys { get; set; }
+    }
+}
---
+++ Models/Toys.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace ToyStore_API.Models
+{
+    public class Toys
+    {
+        public int Id_toy { get; set; }
+    }
+}
