b23018749c030b8bf81abc512caade138b284006
KevinNobre <kevinchristian.nobre@gmail.com>
2025-03-25 20:23:04 -0300

    feat: Anotações DocSwagger - ToyStoreAPI

--- Controllers/ToysController.cs
+++ Controllers/ToysController.cs
@@ -16,25 +16,39 @@ namespace ToyStore_API.Controllers
             _context = context;
         }
 
+        // Get all
+        /// <summary>
+        /// Obter todos os brinquedos
+        /// </summary>
+        /// <returns>Todos os Brinquedos</returns>
+        /// <response code="200">Sucesso</response>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Toys>>> GetAll()
         {
             try
             {
-                return await _context.TB_Toys.ToListAsync();
+                return await _context.Toys.ToListAsync();
             }
             catch (Exception ex)
             {
                 return StatusCode(500, $"Erro interno: {ex.Message}");
             }
         }
-
+        // Get by Id
+        /// <summary>   
+        /// Obter um Paciente
+        /// </summary>
+        /// <param name="id">Identificador do Brinquedo</param>
+        /// <returns>Dados do Brinquedo</returns>
+        /// <response code="200">Sucesso</response>
+        /// <response code="404">Não encontrado</response>
+        /// <response code="500">Erro interno</response>
         [HttpGet("{id}")]
         public async Task<ActionResult<Toys>> GetById(int id)
         {
             try
             {
-                var toy = await _context.TB_Toys.FindAsync(id);
+                var toy = await _context.Toys.FindAsync(id);
                 return toy == null ? NotFound() : toy;
             }
             catch (Exception ex)
@@ -43,6 +57,16 @@ namespace ToyStore_API.Controllers
             }
         }
 
+        // Create
+        /// <summary>
+        /// Cadastrar um Brinquedo
+        /// </summary>
+        /// <remarks>
+        /// objeto Json
+        /// </remarks>
+        /// <returns>Brinquedo recém criado</returns>
+        /// <response code="201">Sucesso</response>
+        /// <response code="404">Não encontrado</response>
         [HttpPost]
         public async Task<ActionResult<Toys>> Create(Toys toy)
         {
@@ -51,7 +75,7 @@ namespace ToyStore_API.Controllers
 
             try
             {
-                _context.TB_Toys.Add(toy);
+                _context.Toys.Add(toy);
                 await _context.SaveChangesAsync();
                 return CreatedAtAction(nameof(GetById), new { id = toy.Id_toy }, toy);
             }
@@ -61,6 +85,14 @@ namespace ToyStore_API.Controllers
             }
         }
 
+        // Update
+        /// <summary>
+        /// Atualizar um Brinquedo
+        /// </summary>
+        /// <param name="id">Identificador do Brinquedo</param>
+        /// <returns>Não retorna informações</returns>
+        /// <response code="404">Não encontrado</response>
+        /// <response code="204">Sucesso</response>
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(int id, Toys toy)
         {
@@ -71,7 +103,7 @@ namespace ToyStore_API.Controllers
 
             try
             {
-                var existingToy = await _context.TB_Toys.FindAsync(id);
+                var existingToy = await _context.Toys.FindAsync(id);
                 if (existingToy == null) return NotFound();
 
                 _context.Entry(toy).State = EntityState.Modified;
@@ -84,15 +116,23 @@ namespace ToyStore_API.Controllers
             }
         }
 
+        // Delete
+        /// <summary>
+        /// Deletar um Brinquedo
+        /// </summary>
+        /// <param name="id">Identificador de Brinquedos</param>
+        /// <returns>Não retorna informações</returns>
+        /// <response code="404">Não encontrado</response>
+        /// <response code="204">Sucesso</response>
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
             try
             {
-                var toy = await _context.TB_Toys.FindAsync(id);
+                var toy = await _context.Toys.FindAsync(id);
                 if (toy == null) return NotFound();
 
-                _context.TB_Toys.Remove(toy);
+                _context.Toys.Remove(toy);
                 await _context.SaveChangesAsync();
                 return NoContent();
             }
