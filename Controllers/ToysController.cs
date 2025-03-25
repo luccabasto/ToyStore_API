93247045cfdd3650b36d494ddf9608f5353ad913
Lucas Basto <136653403+luccabasto@users.noreply.github.com>
2025-03-25 13:27:17 -0300

    feat: crud for api with validator in db

---
+++ Controllers/ToysController.cs
@@ -0,0 +1,69 @@
+﻿using ToyStore_API.Data;
+using ToyStore_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToyStore_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToysController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ToysController(AppDbContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Toys>>> GetAll()
+        {
+            return await _context.TB_Toys.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Toys>> GetById(int id)
+        {
+            var toy = await _context.TB_Toys.FindAsync(id);
+            return toy == null ? NotFound() : toy;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Toys>> Create(Toys toy)
+        {
+            if (toy.Classification_toy > 12)
+                return BadRequest("Só é permitido brinquedos para crianças até 12 anos.");
+
+            _context.TB_Toy.Add(toy);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = toy.Id_toy }, toy);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Toys toy)
+        {
+            if (id != toy.Id_toy) return BadRequest();
+
+            if (toy.Classification_toy > 12)
+                return BadRequest("Classificação acima de 12 anos não é permitida.");
+
+            _context.Entry(toy).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var toy = await _context.TB_Toys.FindAsync(id);
+            if (toy == null) return NotFound();
+
+
+            _context.TB_Toys.Remove(toy);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+    }
+}
--- Models/Toys.cs
+++ Models/Toys.cs
@@ -6,5 +6,20 @@ namespace ToyStore_API.Models
     public class Toys
     {
         public int Id_toy { get; set; }
+
+        [Required]
+        public string Name_toy { get; set; }
+
+        [Required]
+        public string Type_toy { get; set; }
+
+        [Required]
+        [Range(0, 12, ErrorMessage = "Classificação do brinquedo deve ser entre 0 e 12 anos.")]
+        public int Classification_toy { get; set; }
+
+        [Required]
+        public string Brand_toy { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Preço deve ser maior que 0.")]
     }
 }
