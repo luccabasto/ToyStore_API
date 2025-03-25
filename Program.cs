eac9d0bbc6de793eee836d2e1a6d511dfdf1697c
KevinNobre <kevinchristian.nobre@gmail.com>
2025-03-25 16:57:49 -0300

    feat: Configurando SwaggerDoc

--- Program.cs
+++ Program.cs
@@ -1,14 +1,32 @@
 using ToyStore_API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+// Configuraçao do Banco de Dados Oracle
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseOracle(builder.Configuration.GetConnectionString("ToyStoreConnection")));
+
+// Configuração do Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ToyStore API",
+        Version = "v1",
+        Description = "Este sistema realiza cadastro de Brinquedos para a empresa, permitindo a gestão de informações como nome, tipo, classificação, tamanho e preço dos brinquedos.",
+        Contact = new OpenApiContact
+        {
+            Name = "ToyStore API",
+            Url = new Uri("https://github.com/luccabasto/ToyStore_API")
+        }
+    });
+
+});
 
 var app = builder.Build();
 
@@ -19,6 +37,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
